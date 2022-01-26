6823437f4ec165ee74a810cf2726cced15a6147e
rajatup1 <61187216+rajatup1@users.noreply.github.com>
2022-01-26 16:13:36 +0530

    Create my-ec2.tf

---
+++ my-ec2.tf
@@ -0,0 +1,9 @@
+rovider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "my-ec2" {
+    ami = "ami-0af25d0df86db00c1"
+    instance_type = "t2.micro"
+    key_name = "jenkins-test-key"
+}
